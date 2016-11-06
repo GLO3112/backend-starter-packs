e4c9f10250fd5c55ba840101338d08c3e30ebead
Vincent Séguin <vincent.seguin.1@ulaval.ca>
2016-11-06 16:31:47 -0500

    ! Add missing dependency for MySql

--- java-starter/com.glo3112/src/main/java/com/glo3102/SampleApplication.java
+++ java-starter/com.glo3112/src/main/java/com/glo3102/SampleApplication.java
@@ -3,16 +3,15 @@ package com.glo3102;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 // NOTE: For including the database configuration, you need to setup the proper urls in your config file.
-// See https://github.com/netgloo/spring-boot-samples/tree/master/spring-boot-mysql-springdatajpa-hibernate for a very good example.
+// See application.properties file at https://github.com/netgloo/spring-boot-samples/tree/master/spring-boot-mysql-springdatajpa-hibernate for a very good example.
 @ComponentScan
-@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
-public class SampleApplication
-{
-    public static void main(String[] args)
-    {
-        SpringApplication.run(SampleApplication.class, args);
-    }
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
+public class SampleApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SampleApplication.class, args);
+	}
 }
